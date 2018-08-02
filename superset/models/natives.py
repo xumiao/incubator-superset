2389f6a85efe48b961d833f76132b929b82ccfd8
xumiao <pacificxumiao@gmail.com>
2018-08-02 16:21:05 -0700

    changing natives to single class

--- superset/models/natives.py
+++ superset/models/natives.py
@@ -4,81 +4,174 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
-from textwrap import dedent
-
-from future.standard_library import install_aliases
-
-from flask_appbuilder import Model
-
-from sqlalchemy import Column, Integer, String, ForeignKey, Text, Boolean, exists
-from sqlalchemy import Table
-from sqlalchemy.ext.declarative import declared_attr
-
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.hybrid import hybrid_property
 
 from superset import app, db
 from superset.models.norm import Lambda, Variable
 
-from superset import security_manager as sm
-
-from superset.models.helpers import AuditMixinNullable
-from superset.models.mixins import VersionedMixin, lazy_property, ParametrizedMixin
-from superset.models.core import metadata
-
-from pandas import DataFrame
 import traceback
 import logging
 logger = logging.getLogger(__name__)
 
-install_aliases()
 
-config = app.config
-
-
-class RegisterLambda:
-    """
-    A class decorator to ensure the class to be a singleton and register the class in database
-    """
-    def __init__(self, klass):
-        self.klass = klass
-        self.instance = None
-
-    def __call__(self, *args, **kwds):
-        if self.instance is None:
-            self.instance = self.klass(*args, **kwds)
-            instance = db.session.query(self.klass).filter(self.klass.signature == self.instance.signature).first()
-            if not instance:
-                # If the instance does not exist, commit it to the database
-                db.session.add(self.instance)
-                db.commit()
-            else:
-                # Otherwise use the one loaded from database
-                self.instance = instance
-        return self.instance
-
-
-@RegisterLambda
 class NativeLambda(Lambda):
-
     __mapper_args__ = {
         'polymorphic_identity': 'lambda_native'
     }
 
-    def __init__(self, name, version, description):
-        super(Lambda, self).__init__(namespace='norm.native',
+    def __init__(self, name, version, description, variables):
+        super(Lambda, self).__init__(namespace='norm.natives',
                                      name=name,
                                      version=version,
                                      description=description,
-                                     variables=[Variable('value', T)])
+                                     variables=variables)
 
 
-T = NativeLambda(name='Type', version=1, description='A logical function')
-BO = NativeLambda(name='Boolean', version=1, description='Boolean, true/false, 1/0')
-IN = NativeLambda(name='Integer', version=1, description='Integer, -inf..+inf')
-ST = NativeLambda(name='String', version=1, description='String, "blahblah"')
-UN = NativeLambda(name='Unicode', version=1, description='Unicode, u"blahblah"')
-PA = NativeLambda(name='Pattern', version=1, description='Pattern, r"^test[0-9]+"')
-UU = NativeLambda(name='UUID', version=1, description='UUID, $"sfsfsfsf"')
-UR = NativeLambda(name='URL', version=1, description='URL, l"http://example.com"')
-DA = NativeLambda(name='Datetime', version=1, description='Datetime, t"2018-09-01"')
-Any = NativeLambda(name='Any', version=1, description='Any type')
+class TypeLambda(NativeLambda):
+    """
+    A Type relation returns a Lambda object
+    """
+
+    def __init__(self):
+        super(TypeLambda, self).__init__(name='Type',
+                                         version=1,
+                                         description='A logical function',
+                                         variables=[])
+
+    @property
+    def signature(self):
+        return 'Type'
+
+
+class BooleanLambda(NativeLambda):
+    def __init__(self):
+        super(BooleanLambda, self).__init__(name='Boolean',
+                                            version=1,
+                                            description='Boolean, true/false, 1/0',
+                                            variables=[])
+
+    @property
+    def signature(self):
+        return 'Boolean'
+
+
+class IntegerLambda(NativeLambda):
+    def __init__(self):
+        super(IntegerLambda, self).__init__(name='Integer',
+                                            version=1,
+                                            description='Integer, -inf..+inf',
+                                            variables=[])
+
+    @property
+    def signature(self):
+        return 'Integer'
+
+
+class StringLambda(NativeLambda):
+    def __init__(self):
+        super(StringLambda, self).__init__(name='String',
+                                           version=1,
+                                           description='String, "blahbalh"',
+                                           variables=[])
+
+    @property
+    def signature(self):
+        return 'String'
+
+
+class UnicodeLambda(NativeLambda):
+    def __init__(self):
+        super(UnicodeLambda, self).__init__(name='Unicode',
+                                            version=1,
+                                            description='Unicode, u"blahblah"',
+                                            variables=[])
+
+    @property
+    def signature(self):
+        return 'Unicode'
+
+
+class PatternLambda(NativeLambda):
+    def __init__(self):
+        super(PatternLambda, self).__init__(name='Pattern',
+                                            version=1,
+                                            description='Pattern, r"^test[0-9]+"',
+                                            variables=[])
+
+    @property
+    def signature(self):
+        return 'Pattern'
+
+
+class UUIDLambda(NativeLambda):
+    def __init__(self):
+        super(UUIDLambda, self).__init__(name='UUID',
+                                         version=1,
+                                         description='UUID, $"sfsfsfsf"',
+                                         variables=[])
+
+    @property
+    def signature(self):
+        return 'UUID'
+
+
+class URLLambda(NativeLambda):
+    def __init__(self):
+        super(URLLambda, self).__init__(name='URL',
+                                        version=1,
+                                        description='URL, l"http://example.com"',
+                                        variables=[])
+
+    @property
+    def signature(self):
+        return 'URL'
+
+
+class DatetimeLambda(NativeLambda):
+    def __init__(self):
+        super(DatetimeLambda, self).__init__(name='Datetime',
+                                             version=1,
+                                             description='Datetime, t"2018-09-01"',
+                                             variables=[])
+
+    @property
+    def signature(self):
+        return 'Datetime'
+
+
+class AnyLambda(NativeLambda):
+    def __init__(self):
+        super(AnyLambda, self).__init__(name='Any',
+                                        version=1,
+                                        description='Any type',
+                                        variables=[])
+
+    @property
+    def signature(self):
+        return 'Any'
+
+
+class TensorLambda(NativeLambda):
+    __mapper_args__ = {
+        'polymorphic_identity': 'lambda_native_tensor'
+    }
+
+    dtype = Column(String(16), default='float32')
+    shape = Column(String(128), default='(300,')
+
+    def __init__(self, dtype, shape):
+        super(TensorLambda, self).__init__(name='Tensor',
+                                           version=1,
+                                           description='Tensor, [2.2, 3.2]',
+                                           variables=[])
+        self.dtype = dtype
+        self.shape = str(shape)
+
+    @hybrid_property
+    def dim(self):
+        return len(eval(self.shape))
+
+    @property
+    def signature(self):
+        return 'Tensor[{}]{}'.format(self.dtype, self.shape)
