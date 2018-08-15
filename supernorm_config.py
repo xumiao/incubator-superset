6d8f4c3cbf9bd4a5b147b23201e9fbf479149274
xumiao <pacificxumiao@gmail.com>
2018-08-14 22:13:17 -0700

    enable google sign-in

--- supernorm_config.py
+++ supernorm_config.py
@@ -6,6 +6,8 @@ from __future__ import unicode_literals
 
 import os
 
+from flask_appbuilder.const import AUTH_OAUTH
+
 
 def get_env_variable(var_name, default=None):
     """Get the environment variable or raise exception."""
@@ -54,11 +56,32 @@ class CeleryConfig(object):
 
 CELERY_CONFIG = CeleryConfig
 
+CSRF_ENABLED = True
+
+AUTH_TYPE = AUTH_OAUTH
 AUTH_USER_REGISTRATION = True
+AUTH_USER_REGISTRATION_ROLE = "Public"
 RECAPTCHA_USE_SSL = True
 RECAPTCHA_PUBLIC_KEY = '6LfqG2cUAAAAAGvRGZnMC5-wNsITHuq1HjVLSlB7'
 RECAPTCHA_PRIVATE_KEY = '6LfqG2cUAAAAAOPMCcOeXBVllXbsS0-cTGjzIdiH'
 RECAPTCHA_OPTIONS = {'theme': 'white'}
+
+OAUTH_PROVIDERS = [{
+   'name': 'google',
+   'icon': 'fa-google',
+   'token_key': 'access_token',
+   'remote_app': {
+     'base_url': 'https://www.googleapis.com/oauth2/v2/',
+     'request_token_params': {
+        'scope': 'email profile'
+     },
+     'request_token_url': None,
+     'access_token_url': 'https://accounts.google.com/o/oauth2/token',
+     'authorize_url': 'https://accounts.google.com/o/oauth2/auth',
+     'consumer_key': '62152624246-89s61p6imb5l8e2adc50td88f9oni4o2.apps.googleusercontent.com',
+     'consumer_secret': 'gEOpH4jX8hjOjhhk1shRs6uj'
+    }
+}]
 
 APP_NAME = 'SuperNorm'
 APP_ICON = '/static/assets/images/supernorm-logo@2x.png'
